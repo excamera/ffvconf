6eee194f33d507e2499c8e350b164137ad155edf
Sadjad Fouladi <sfouladi@gmail.com>
2017-07-27 13:31:50 -0700

    Create sender script

---
+++ sender.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+
+CAMERA_DEVICE=/dev/video0
+FRAME_RATE=10
+VIDEO_SIZE=1280x720
+VIDEO_FILE=/tmp/myvideo0
+
+#mplayer -benchmark "ffmpeg://udp://0.0.0.0:2112?listen&buffer_size=10240" &
+
+#sleep 1
+
+ffmpeg -f v4l2 -framerate ${FRAME_RATE} -video_size ${VIDEO_SIZE} -pix_fmt yuyv422 -i /dev/video0 \
+  -c:v libx264 -preset veryfast -tune zerolatency -pix_fmt yuv420p \
+  -x264opts crf=20:vbv-maxrate=3000:vbv-bufsize=100:intra-refresh=1:slice-max-size=1500:keyint=1:ref=1 \
+  -f mpegts - | nc -l -p 9000
